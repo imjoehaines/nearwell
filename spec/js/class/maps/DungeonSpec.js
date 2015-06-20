e71b5c4cb870111fca2fdccea90bb5be188f6781
Joe Haines <hello@joehaines.co.uk>
2015-06-20 21:02:39 +0100

    Width & height tests for dungeon class

--- spec/js/class/maps/DungeonSpec.js
+++ spec/js/class/maps/DungeonSpec.js
@@ -14,4 +14,14 @@ describe('Dungeon', function () {
   it('should extend BaseMap', function () {
     expect(new Dungeon() instanceof BaseMap).toBe(true)
   })
+
+  it('should have a default width and height', function () {
+    expect(new Dungeon().width).toBe(100)
+    expect(new Dungeon().height).toBe(100)
+  })
+
+  it('should have set a given width and height', function () {
+    expect(new Dungeon({width: 10}).width).toBe(10)
+    expect(new Dungeon({height: 20}).height).toBe(20)
+  })
 })
