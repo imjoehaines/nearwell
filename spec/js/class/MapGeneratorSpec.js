f7b9e64a5c6bf23e169f3a0680f35c4679fd24f3
Joe Haines <hello@joehaines.co.uk>
2015-06-20 11:11:22 +0100

    Added tests for setting width & height

--- spec/js/class/MapGeneratorSpec.js
+++ spec/js/class/MapGeneratorSpec.js
@@ -11,27 +11,37 @@ describe('MapGenerator', function () {
     expect(new MapGenerator() instanceof MapGenerator).toBe(true)
   })
 
-  it('should set a width', function () {
-    expect(new MapGenerator().width).toBe(100)
-  })
-
-  it('should set a height', function () {
-    expect(new MapGenerator().height).toBe(100)
-  })
-
-  describe('Map types', function () {
-    it('should set a default map type', function () {
-      expect(new MapGenerator().getMapType()).toBe('dungeon')
+  describe('Options', function () {
+    it('should set a default width', function () {
+      expect(new MapGenerator().width).toBe(100)
     })
 
-    it('should set a map type', function () {
-      expect(new MapGenerator({mapType: 'dungeon'}).getMapType()).toBe('dungeon')
+    it('should set a width when given', function () {
+      expect(new MapGenerator({width: 20}).width).toBe(20)
     })
 
-    it('should throw an error if given an invalid map type', function () {
-      expect(function () {
-        new MapGenerator({mapType: 'no way'}) // eslint-disable-line no-new
-      }).toThrow()
+    it('should set a default height', function () {
+      expect(new MapGenerator().height).toBe(100)
+    })
+
+    it('should set a height when given', function () {
+      expect(new MapGenerator({height: 2000}).height).toBe(2000)
+    })
+
+    describe('Map types', function () {
+      it('should set a default map type', function () {
+        expect(new MapGenerator().getMapType()).toBe('dungeon')
+      })
+
+      it('should set a map type', function () {
+        expect(new MapGenerator({mapType: 'dungeon'}).getMapType()).toBe('dungeon')
+      })
+
+      it('should throw an error if given an invalid map type', function () {
+        expect(function () {
+          new MapGenerator({mapType: 'no way'}) // eslint-disable-line no-new
+        }).toThrow()
+      })
     })
   })
 
